058b5df6f855fe7a493bff605f6edf7f07f5d09b
Bill Chan <billpwchan@hotmail.com>
2022-07-20 00:59:50 +0800

    Update Files

--- node_script.ps1
+++ node_script.ps1
@@ -1,5 +1,5 @@
 do{
-    Set-Location -Path C:\Users\Administrator\Desktop\Strategy-Trading-Scraper
+    Set-Location -Path C:\Users\billpwchan\Desktop\Strategy-Trading-Scraper
     node ./index.js
     git add .
     git commit -m "Update Files"
